5afa45189b06258f8c8df017ff795680262175ff
Arthur Guilarde <arthur.nemi.g@gmail.com>
2021-02-11 12:20:43 -0300

    Routes

--- backend-node/adonis-api/project/app/Controllers/Http/SessionController.js
+++ backend-node/adonis-api/project/app/Controllers/Http/SessionController.js
@@ -2,7 +2,7 @@
 
 
 class SessionController {
-  async index ({ request, response, auth }) {
+  async create ({ request, response, auth }) {
     const data = request.only(['login', 'password'])
 
     const token = await auth.attempt(data.login, data.password)
--- backend-node/adonis-api/project/start/routes.js
+++ backend-node/adonis-api/project/start/routes.js
@@ -20,11 +20,11 @@ Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
 
-Route.get('/login', 'SessionController.index')
-Route.get('/products', 'ProductController.index')
-Route.get('/status', 'StatusController.index')
-Route.get('/uTypes', 'UserTypeController.index')
-Route.post('/users', 'UserController.store')
+Route.get('/login', 'SessionController.create').as('login.create')
+Route.get('/products', 'ProductController.index').as('products.index')
+Route.get('/status', 'StatusController.index').as('status.index')
+Route.get('/uTypes', 'UserTypeController.index').as('uTypes.index')
+Route.post('/users', 'UserController.store').as('users.store')
 
 Route.group(() => {
   Route.resource('instructors', 'InstructorController').apiOnly()
